e7aa94751f030c36d5f6a2df0615ed3fce4b2789
JP <40834157+TheSirYeti@users.noreply.github.com>
2020-12-18 00:29:16 -0300

    Dialogue v1
    Text implementation and a working dialogue skip

--- Assets/Scripts/UI/DialogueSystem/Speech.cs
+++ Assets/Scripts/UI/DialogueSystem/Speech.cs
@@ -13,6 +13,9 @@ public class Speech : MonoBehaviour
     public Text nameText;
     public Animator animator;
     public Image[] images;
+    public bool skip = false;
+    private bool isTyping;
+    public List<int> imagePerSentence = new List<int>();
 
     void Start(){
         StartCoroutine(TypeSentence());
@@ -21,27 +24,51 @@ public class Speech : MonoBehaviour
 
     void Update(){
         if(Input.GetKeyDown("space")){
-            NextSentence();
+            if(!skip){
+                textBox.text = "";
+                int counter = 0;
+                foreach(char letter in sentences[index].ToCharArray()){
+                    if(counter >= 69){
+                        textBox.text += "\n";
+                        counter = 0;
+                    }
+                    textBox.text += letter;
+                    counter++;
+                    skip = true;
+                    isTyping = false;
+                }
+            } else if(isTyping == false){
+                NextSentence();
+            }
         }
+        
     }
 
     IEnumerator TypeSentence(){
+        isTyping = true;
+        print(index);
         int counter = 0;
         foreach(char letter in sentences[index].ToCharArray()){
-            if(counter >= 69){
-                textBox.text += "\n";
-                counter = 0;
+            if(!skip){
+                if(counter >= 69){
+                    textBox.text += "\n";
+                    counter = 0;
+                }
+                textBox.text += letter;
+                counter++;
+                yield return new WaitForSeconds(typingSpeed);
             }
-            textBox.text += letter;
-            counter++;
-            yield return new WaitForSeconds(typingSpeed);
         }
+        isTyping = false;
+        skip = true;
+
     }
 
     public void NextSentence(){
         if(index < sentences.Count - 1){
             index++;
             textBox.text = "";
+            skip = false;
             StartCoroutine(TypeSentence());
         } else {
             textBox.text = "";
